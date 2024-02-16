febfc063591b1c1e167dbd5bf9a9eaaf8bf3fb21
Abhishek khare <ghostabhi323@gmail.com>
2024-02-16 15:39:58 +0530

    CORS Updated

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from auth import router as auth_router
 from blog import router as blog_router
 app = FastAPI()
+
+# Define a list of allowed origins for CORS
+# You can use ["*"] to allow all origins
+allowed_origins = [
+    "http://localhost:3000",  # Allow frontend application hosted on localhost:3000
+]
+
+# Add CORSMiddleware to the application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,  # Specify the allowed origins
+    allow_credentials=True,  # Allow cookies to be included in cross-origin requests
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 
 app.include_router(auth_router)
